61c1cb10c7eeaeaab3fe477f8e90a3497bdb1e7d
Anya <anyaanto1909@gmail.com>
2020-03-05 00:00:09 -0500

    updated src

---
+++ src/resumeData.js
@@ -0,0 +1,83 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Anya Fareena",
+    "role": "GSuite Solutions Developer",
+    "Email":"anyafareena@gmail.com",
+    "roleDescription": "Dynamic and motivated IT Professional with 8+ years of experience in Financial Organization, Results-driven and customer focused building user-friendly GSuite applications",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/anya-fareena-330192109/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/AnyaFareena",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am currently working at Capital One as GSuite Solutions Developer, helping users bridge the gap and open new possibilities to the world of Google",
+    "address":"Toronto, ON",
+    "website":"https://anyafareena.github.io/Portfolio/",
+    "education":[
+      {
+        "UniversityName":"Mangalore University",
+        "specialization":"Computer Applications",
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Capital One",
+        "specialization":"GSuite Products",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2018",
+      },
+
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Google Apps Script"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Python"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"NoteTaker",
+        "imgurl":"images/portfolio/note.png"
+      },
+      {
+        "name":"project2",
+        "description":"Quiz Journey",
+        "imgurl":"images/portfolio/quiztime.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"Day Planner",  
+        "imgurl":"images/portfolio/day.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"Fitness Tracker",
+        "imgurl":"images/portfolio/fitness-tracker.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"abc"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"xyz"
+      }
+    ]
+  }
+  
+  export default resumeData
